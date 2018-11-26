8508a51dca731922e28404e2a791cf36815108c5
Rex Lo <LORE2@OOCL.COM>
2018-11-26 21:49:51 +0800

    Refactored BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,10 +1,38 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
-    void renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            // do something
+    private String platform;
+    private String browser;
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public BannerRender(String platform, String browser){
+        this.platform = platform;
+        this.browser = browser;
+    }
+
+    public void renderBanner() {
+        if (doesPlatformHasMAC(platform) && doesBrowserHasIE(browser)) {
+            doSomething();
         }
     }
+
+    boolean doesPlatformHasMAC(String platform){
+        return platform.toUpperCase().contains("MAC");
+    }
+
+    boolean doesBrowserHasIE(String browser){
+        return browser.toUpperCase().contains("IE");
+    }
+
+    void doSomething(){
+        this.platform = "Window";
+    }
+
 }
--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -1,12 +1,52 @@
 package com.tws.refactoring.extract_variable;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BannerRenderTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
 
     @Test
     void renderBanner() {
+        BannerRender bannerRender = new BannerRender("MAC", "IE");
+        bannerRender.renderBanner();
+        assertEquals("Window", bannerRender.getPlatform());
+    }
+
+    @Test
+    void doesPlatformHasMAC() {
+        BannerRender bannerRender = new BannerRender("MAC", "IE");
+        assertTrue(bannerRender.doesPlatformHasMAC(bannerRender.getPlatform())) ;
+    }
+
+    @Test
+    void doesBrowserHasIE() {
+        BannerRender bannerRender = new BannerRender("MAC", "IE");
+        assertTrue(bannerRender.doesBrowserHasIE(bannerRender.getBrowser()));
+    }
+
+    @Test
+    void doSomething() {
+        BannerRender bannerRender = new BannerRender("MAC", "IE");
+        bannerRender.doSomething();
+        assertEquals("Window", bannerRender.getPlatform());
     }
 }
